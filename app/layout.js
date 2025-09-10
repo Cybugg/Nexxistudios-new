e21ebf60f9e76b8a7696e4e604cd3a53871ab9ce
Oguntimehin samuel <timehinsamuel11@gmail.com>
2025-09-10 10:33:48 +0300

    correct grammar

--- app/layout.js
+++ app/layout.js
@@ -20,13 +20,13 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Nexxi Studios: Bye to All Mid Websites and Softwares",
+  title: "Nexxi Studios: Bye to All Mid Websites and Software",
   description: "We are a future-focused software development and creative web agency, specializing in custom software solutions, modern website design, SEO optimization, and website redesigns. Our team partners with top designers and developers to build innovative digital platforms, rescue outdated websites, and help brands grow online",
   icons: {
     icon: "/icon.ico",
   },
   openGraph: {
-    title: "Nexxi Studios: Bye to All Mid Websites and Softwares",
+    title: "Nexxi Studios: Bye to All Mid Websites and Software",
     description: "We are a future-focused software development and creative web agency, specializing in custom software solutions, modern website design, SEO optimization, and website redesigns. Our team partners with top designers and developers to build innovative digital platforms, rescue outdated websites, and help brands grow online",
     url: "https://www.nexxistudios.com/",
     siteName: "nexxistudios",
@@ -42,7 +42,7 @@ export const metadata = {
   },
   twitter: { 
     card: "summary_large_image",
-    title: "Nexxi Studios: Bye to All Mid Websites and Softwares",
+    title: "Nexxi Studios: Bye to All Mid Websites and Software",
     description: "We are a future-focused software development and creative web agency, specializing in custom software solutions, modern website design, SEO optimization, and website redesigns. Our team partners with top designers and developers to build innovative digital platforms, rescue outdated websites, and help brands grow online",
     images: ["/logo.png"],
   },
